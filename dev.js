248ddc5f49ca5769390343856d11713f78cb7075
Daniyal Ahmed <daniyal@bu.edu>
2026-06-15 06:57:18 -0500

    FINALLY FUCKING DONEE

---
+++ dev.js
@@ -0,0 +1,9 @@
+const Path = require("path");
+
+module.exports.config = {
+  Port: 3333,
+  ConfigPath: Path.resolve("config"),
+  MetadataPath: Path.resolve("metadata"),
+  FFmpegPath: "/usr/bin/ffmpeg",
+  FFProbePath: "/usr/bin/ffprobe"
+};
